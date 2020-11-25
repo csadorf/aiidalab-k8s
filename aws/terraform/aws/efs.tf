65d7461c723129ed6268feb062c8ecc39f507d18
Simon Adorf <simon.adorf@epfl.ch>
2020-11-25 16:18:46 +0100

    Fixes helm repository urls.

--- aws/terraform/aws/efs.tf
+++ aws/terraform/aws/efs.tf
@@ -34,11 +34,6 @@ resource "aws_efs_mount_target" "home_dirs_targets" {
   security_groups = [ aws_security_group.home_dirs_sg.id ]
 }
 
-data "helm_repository" "stable" {
-  name = "stable"
-  url = "https://kubernetes-charts.storage.googleapis.com"
-}
-
 resource "kubernetes_namespace" "support" {
   metadata {
     name = "support"
@@ -48,7 +43,7 @@ resource "kubernetes_namespace" "support" {
 resource "helm_release" "efs-provisioner" {
   name = "efs-provisioner"
   namespace = kubernetes_namespace.support.metadata.0.name
-  repository = data.helm_repository.stable.metadata[0].name
+  repository = "https://charts.helm.sh/stable/"
   chart = "efs-provisioner"
   version = "0.11.0"
 
